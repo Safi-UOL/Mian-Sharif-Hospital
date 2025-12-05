b8ae8f80e715d5ae4009678ec571e92ca5623424
Your Name <7014030u@student.uol.edu.pk>
2025-12-05 09:03:50 +0500

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const isProd = process.env.NODE_ENV === "production";
-
 export default defineConfig({
-  base: isProd ? "/Mian-Sharif-Hospital/" : "/",
+  base: "/Mian-Sharif-Hospital/",
   plugins: [react()],
 });
